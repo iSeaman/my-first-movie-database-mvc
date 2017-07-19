03107c20a9913d0966b60c1e642e87b832415ac0
Ilona Seaman <i.seaman@outlook.com>
2017-07-19 08:57:49 -0400

    created website from scratch - this is version one

--- MyFirstMovieApp/Controllers/HomeController.cs
+++ MyFirstMovieApp/Controllers/HomeController.cs
@@ -3,28 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using MyFirstMovieApp.Models;
+using System.Data.Entity.Migrations;
 
 namespace MyFirstMovieApp.Controllers
 {
     public class HomeController : Controller
     {
+        private ilonaMoviesEntities _db = new ilonaMoviesEntities();
+        // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Movies1.ToList());
         }
 
+        // GET: Home/About
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
         {
-            ViewBag.Message = "Your contact page.";
+            var movieToView = (from m in _db.Movies1
+                               where m.Id == id
+                               select m).First();
+            return View(movieToView);
+        }
 
+        // GET: Home/Create
+        public ActionResult Create()
+        {
             return View();
         }
+
+        // POST: Home/Create
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create([Bind(Exclude ="Id")] Movie movieToCreate)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (!ModelState.IsValid)
+                {
+                    return View();
+                }
+                _db.Movies1.Add(movieToCreate);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var movieToEdit = (from m in _db.Movies1
+                               where m.Id == id
+                               select m).First();
+            return View(movieToEdit);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Movie movieToEdit)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(movieToEdit);
+                }
+                _db.Entry(movieToEdit).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
     }
 }
